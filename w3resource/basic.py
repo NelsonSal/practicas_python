a9b03090116c298e44450705d30b4889d6aeba29
NelsonJ <njsocode@gmail.com>
2022-10-15 15:06:16 -0500

    sera que ya?

---
+++ w3resource/basic.py
@@ -0,0 +1,35 @@
+#Imprime la verison del Python
+# import sys
+# print ("MI VERSION DE PYTON ES: ", sys.version, sys.version_info)
+
+#Imprime la fecha hoy
+# from datetime import datetime
+# fecha=datetime.today()
+# print ("Hoy es: ", fecha)
+
+#Convierte numeros separados por comma en lista y en tupla
+# numeros=input("Introduzca los numeros separados por coma: ")
+# lista=[numeros]
+# tupla=eval(numeros)
+# print (lista," ", type(lista))
+# print (tupla," ", type(tupla))
+
+#Muestra la extencion d eun archivo introducido
+# archivo=input("Intro el nombre del archivo con su extención: ")
+# control=0
+# extencion=''
+# for i in(archivo):
+#     if control==1:
+#         extencion+=i
+#     if i=="." :
+#         control=1
+    
+# print("La extención del archivo es: ", extencion) 
+# #otra solucion con operadores de string 
+# otra_sol=archivo.split(".")
+# print("se separa en dos en el punto: ",otra_sol," e imprimimos la ultima: ",otra_sol[-1])
+
+# voy a hacer un commmit acá <--
+# le cambie nombre al file y commit <--
+# Creare una nueva rama y le vy a agregar algo arriba bonito  
+# que pasa?
